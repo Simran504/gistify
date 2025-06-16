8a5315e9dcb85c2974fc07e75fc5c73b3a8a8550
Simran504 <patelsimransp504@gmail.com>
2025-06-16 17:04:06 +0530

    script to extract text

--- popup.js
+++ popup.js
@@ -1,3 +1,15 @@
 document.getElementById("summarize-btn").addEventListener("click", ()=>{
-    console.log("Summarize clicked");
-})
+    const result = document.getElementById("result");
+    result.textContent = "Extracting content....";
+
+    chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
+        chrome.tabs.sendMessage(
+            tab.id, 
+            {type: "GET_ARTICLE_TEXT"}, 
+            ({text}) => {
+                setTimeout(() => {
+                    result.textContent = text ? text.slice(0, 300) + " " : "No article text found.";            
+                }, 1000);   //1 second delay
+        });
+    });
+});
